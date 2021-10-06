1b57ff60ff722f3169e7f0cf7a5b5b60c0e1d194
Carlos <ce-sarr@hotmail.com>
2021-10-06 15:41:35 -0300

    Ajustando carregamento de página ao centro

--- src/components/ThemedSuspense.js
+++ src/components/ThemedSuspense.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 export default function ThemedSuspense() {
   return (
-    <div className="w-full h-screen p-6 text-lg font-medium text-gray-600 dark:text-gray-400 dark:bg-gray-900">
+    <div className="w-full h-screen p-6 flex justify-center items-center text-lg font-medium text-gray-600 dark:text-gray-400 dark:bg-gray-900">
       Carregando...
     </div>
   )
